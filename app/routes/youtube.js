3769c432c37cb3482cf3982376cfaf9f617bb49f
shadid <shadidhaque2014@gmail.com>
2017-07-29 15:05:37 -0400

    search list done now

--- app/routes/youtube.js
+++ app/routes/youtube.js
@@ -1,7 +1,23 @@
+const YouTube = require('youtube-node')
+const youtube = new YouTube()
+youtube.setKey('AIzaSyBSx_9uxsCJ-LdsK1SJKGGZ_CqFyEAlotE')
+
+
 module.exports = (app) => {
 
 app.post('/search', function (req, res) {
-  console.log(req.body)
+  // console.log(req.body.ysearch)
+  youtube.search(req.body.ysearch, 2, function(err, result){
+  	if(err){
+  		console.log(err)
+  	}else{
+  		// var tres = JSON.stringify(result, null, 2)
+  		res.render('result', { res: result.items })
+  		// res.send(result.items)
+  		// console.log(JSON.stringify(result, null, 2))
+  	}
+  })
+
 })
 
 
