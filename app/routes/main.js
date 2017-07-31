853e117d4c425b12ac22cce662a10e8d1c035a8f
Aman Dias <Aman Dias>
2017-07-31 18:17:39 -0400

    ready for socket

--- app/routes/main.js
+++ app/routes/main.js
@@ -3,5 +3,6 @@ module.exports = (app, passport) => {
 require('./user.js')(app, passport)
 require('./youtube.js')(app)
 require('./rooms.js')(app, passport)
+require('./playlist')(app, passport)
 
 }
