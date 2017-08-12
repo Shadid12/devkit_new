bd4bbb3919b2bbe05b919024577e241356e6a4a6
shadid <shadidhaque2014@gmail.com>
2017-08-11 23:12:17 -0400

    somehow figured the yt player

--- public/js/play.js
+++ public/js/play.js
@@ -4,12 +4,30 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
-console.log(aroom);
+var current = $('#nowplaying').val();
+
+
 
 function onYouTubePlayerAPIReady() {
     player = new YT.Player('player', {
       height: '360',
       width: '640',
-      videoId: 'M7lc1UVf-VE'
+      videoId: current,
+      events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
     });
 }
+
+function onPlayerReady(event) {
+        event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+ }
