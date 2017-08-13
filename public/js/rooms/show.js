b36f1380a55bbdd3ef5b15478ee3d2de05ae43bc
shadid <shadidhaque2014@gmail.com>
2017-08-13 13:58:43 -0400

    working non persistant

--- public/js/rooms/show.js
+++ public/js/rooms/show.js
@@ -2,9 +2,43 @@ $(document).ready(function(){
 
 var socket = io.connect();
 
+
 socket.on('someroom', function(msg){
 	console.log(msg);
+	// $("#video")[0].src = "//www.youtube.com/embed/" + msg + "?rel=0/&autoplay=1";
+
+
+
+    // create youtube player
+    var player;
+    player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: msg,
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+    });
+    
+    // autoplay video
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
+
+    // when video ends
+    function onPlayerStateChange(event) {        
+        if(event.data === 0) {            
+        	console.log('video done');
+        }
+    }
+
+
+
 });
+
+
+
 
 
 });
